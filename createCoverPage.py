441e60fad368127381516f1adb0f445ddd949c1d
geethasingam <rajasekarangeetha14@gmail.com>
2019-01-29 07:58:01 +0000

    changed file

--- createCoverPage.py
+++ createCoverPage.py
@@ -4,6 +4,7 @@ import os
 import csv
 import sys
 import time
+import subprocess
 
 # Uploads the html file to the noolaham.net server
 def uploadToServer(i_session, i_ftppath, i_htmldir, i_htmlfile):
@@ -14,7 +15,8 @@ def uploadToServer(i_session, i_ftppath, i_htmldir, i_htmlfile):
      file = open(i_htmlfile,'rb')                  		# file to send
      i_session.storbinary('STOR ' + i_htmlfile, file)     	# send the file
      file.close()                                    	# close file and FTP
-     return 
+     return                                    	      
+    
      
 def downloadFile(i_fileURL):
     command = "wget " + i_fileURL + " -P data"
@@ -22,15 +24,29 @@ def downloadFile(i_fileURL):
     os.system(command)    
                                 
 def createCoverImage(i_fileName):
-    command = "java -jar LinkTester.jar ccp " + i_fileName
-    os.system(command)
+    try:
+	cp_fileName =  i_fileName.replace("pdf", "JPG")
+        command = "convert " + i_fileName + "[0] " + cp_fileName
+        os.system(command)
+    except OSError:
+        print ('Convert failed to create TN.')
+	raise Exception("Convert failed to create TN.")
     
-session = ftplib.FTP('ftpsever','ftpusername','ftppw')
+session = ftplib.FTP('ftpserver','username','password')
 tooldir = r"/var/www/html"
 datadir = os.path.join(tooldir, "data")
 
 #Read through each entry and update wiki
 batchfile = open('createdata.csv', 'rt')
+
+error_file_name = "error_list.txt"
+
+if os.path.exists(error_file_name):
+    append_write = 'a' # append if already exists
+else:
+    append_write = 'w' # make a new file if not
+errorlist = open(error_file_name, append_write)
+
 try:
      reader = csv.reader(batchfile)
      for row in reader:        
@@ -39,12 +55,16 @@ try:
         pgFolderPath = row[1]
         pgURL = "http://noolaham.net" + pgFolderPath + "/" + pgNoolahamNum + ".pdf"
         print "processing " + pgURL
-        downloadFile(pgURL)  
-        createCoverImage("data/" + pgNoolahamNum + ".pdf")
-        uploadToServer(session, pgFolderPath, datadir, pgNoolahamNum + ".JPG")
-  
-finally:
-     batchfile.close()
-     
+        downloadFile(pgURL) 
+        try:
+            createCoverImage("data/" + pgNoolahamNum + ".pdf")
+            uploadToServer(session, pgFolderPath, datadir, pgNoolahamNum + ".JPG")
+        except:
+            errorlist.write(pgNoolahamNum + "\n") 
+            print('An error occured trying to read the file.' + pgNoolahamNum)
 
+finally:
+    batchfile.close()
+            
+        
 session.quit()
